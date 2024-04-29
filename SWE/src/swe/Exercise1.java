03a0271262883cce165f21cc6e63f9c5cc86aca4
phamt <phamthe.30071997@gmailbp.programmieren@gmail.com>
2024-04-30 00:42:19 +0200

    update

--- SWE/src/swe/Exercise1.java
+++ SWE/src/swe/Exercise1.java
@@ -1,9 +1,19 @@
 package swe;
 
 public class Exercise1 {
-
+	static void Ulam(int n) {
+		int t  = 0;
+		int x = n;
+		while (x != 1) {
+			t++;
+			if (x%2 == 0) x=x/2;
+			else x = x/3 + 1;
+		}
+		System.out.println("Function is terminated after " + t + " time(s)");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n = 6;
+		Ulam(n);
 
 	}
 
